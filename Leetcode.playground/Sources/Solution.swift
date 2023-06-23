d8d2a7306ae3b4995862599147d77acef280f9a5
xinyzhao <xinyzhao@qq.com>
2023-06-23 22:10:12 +0800

    Add removeDuplicates

--- Leetcode.playground/Sources/Solution.swift
+++ Leetcode.playground/Sources/Solution.swift
@@ -631,4 +631,16 @@ public class Solution {
         tail?.next = reverseKGroup(node, k)
         return head
     }
+    
+    public func removeDuplicates(_ nums: inout [Int]) -> Int {
+        if nums.isEmpty { return 0 }
+        var i = 0
+        for j in 1 ..< nums.count {
+            if nums[i] != nums[j]  {
+                i += 1
+                nums[i] = nums[j]
+            }
+        }
+        return i + 1
+    }
 }
