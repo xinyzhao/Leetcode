b2e56d85a3858f27e4cc166b225c49e34300423f
xinyzhao <xinyzhao@qq.com>
2023-06-27 00:32:44 +0800

    Add solveSudoku

--- Leetcode/Sources/Solution.swift
+++ Leetcode/Sources/Solution.swift
@@ -930,7 +930,7 @@ class Solution {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
         var l = 0, r = nums.count - 1
         while l <= r {
-            var mid = (l + r) / 2
+            let mid = (l + r) / 2
             if target < nums[mid] {
                 r = mid - 1
             } else if target > nums[mid] {
@@ -943,38 +943,127 @@ class Solution {
     }
     
     func isValidSudoku(_ board: [[Character]]) -> Bool {
-        var rows = [Int:[Character]]()
-        var cols = [Int:[Character]]()
-        var boxes = [Int:[Character]]()
+        var lines = [Int:[Character]]()
+        var columns = [Int:[Character]]()
+        var blocks = [Int:[Character]]()
         for i in 0 ..< board.count {
-            let line = board[i]
-            for j in 0 ..< line.count {
-                let c = line[j]
+            let row = board[i]
+            for j in 0 ..< row.count {
+                let c = row[j]
                 if c == "." { continue }
                 //
-                var row = rows[i] ?? [Character]()
-                if row.contains(c) {
+                var line = lines[i] ?? [Character]()
+                if line.contains(c) {
                     return false
                 }
-                row.append(c)
-                rows[i] = row
+                line.append(c)
+                lines[i] = line
                 //
-                var col = cols[j] ?? [Character]()
-                if col.contains(c) {
+                var column = columns[j] ?? [Character]()
+                if column.contains(c) {
                     return false
                 }
-                col.append(c)
-                cols[j] = col
+                column.append(c)
+                columns[j] = column
                 //
                 let k = (i / 3) * 3 + j / 3
-                var box = boxes[k] ?? [Character]()
-                if box.contains(c) {
+                var block = blocks[k] ?? [Character]()
+                if block.contains(c) {
                     return false
                 }
-                box.append(c)
-                boxes[k] = box
+                block.append(c)
+                blocks[k] = block
             }
         }
         return true
     }
+    
+    func solveSudoku(_ board: inout [[Character]]) {
+        let grids = solveSudokuGrids(&board)
+        for grid in grids {
+            for c in grid.values {
+                var copy = board
+                copy[grid.x][grid.y] = c
+                let next = solveSudokuGrids(&copy)
+                if next.isEmpty, isValidSudoku(copy) {
+                    board = copy
+                    return
+                }
+            }
+        }
+    }
+    
+    class SudokuBlankGrid {
+        var x: Int = 0
+        var y: Int = 0
+        var z: Int { (x / 3) * 3 + y / 3 }
+        var values = [Character]()
+        init() {}
+        init(x: Int, y: Int) { self.x = x; self.y = y; }
+        func merge(_ line: [Character], _ column: [Character], _ block: [Character]) {
+            values.removeAll()
+            for c in block {
+                if line.contains(c), column.contains(c) {
+                    values.append(c)
+                }
+            }
+        }
+    }
+    
+    func solveSudokuGrids(_ board: inout [[Character]]) -> [SudokuBlankGrid]  {
+        let temps: [Character] = ["1","2","3","4","5","6","7","8","9"]
+        var lines: [[Character]] = {
+            var list = [[Character]]()
+            for _ in 0 ..< temps.count {
+                list.append(temps)
+            }
+            return list
+        }()
+        var columns: [[Character]] = {
+            var list = [[Character]]()
+            for _ in 0 ..< temps.count {
+                list.append(temps)
+            }
+            return list
+        }()
+        var blocks: [[Character]] = {
+            var list = [[Character]]()
+            for _ in 0 ..< temps.count {
+                list.append(temps)
+            }
+            return list
+        }()
+        var girds = [SudokuBlankGrid]()
+        for x in 0 ..< board.count {
+            let line = board[x]
+            for y in 0 ..< line.count {
+                let c = line[y]
+                if c == "." {
+                    girds.append(SudokuBlankGrid(x: x, y: y))
+                    continue
+                }
+                if let i = lines[x].firstIndex(of: c) {
+                    lines[x].remove(at: i)
+                }
+                if let i = columns[y].firstIndex(of: c) {
+                    columns[y].remove(at: i)
+                }
+                let z = (x / 3) * 3 + y / 3
+                if let i = blocks[z].firstIndex(of: c) {
+                    blocks[z].remove(at: i)
+                }
+            }
+        }
+        var recur = false
+        for gird in girds {
+            gird.merge(lines[gird.x], columns[gird.y], blocks[gird.z])
+            if gird.values.count == 1 {
+                board[gird.x][gird.y] = gird.values[0]
+                recur = true
+            }
+        }
+        return recur ? solveSudokuGrids(&board) : girds
+    }
+    
 }
+
