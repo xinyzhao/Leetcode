cc1454e28fc032d41b43c89c1c55265d681b6b57
xinyzhao <xinyzhao@qq.com>
2023-07-07 16:06:44 +0800

    Add mySqrt

--- Leetcode/Sources/Solution.swift
+++ Leetcode/Sources/Solution.swift
@@ -1903,5 +1903,20 @@ class Solution {
         }
         return strs
     }
+    
+    func mySqrt(_ x: Int) -> Int {
+        if x <= 1 { return x }
+        var l = 1, r = x / 2, mid = 1, ret = 0
+        while l <= r {
+            mid = l + (r - l) / 2
+            if mid * mid <= x {
+                ret = mid
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+        return ret
+    }
 }
 
--- LeetcodeTests/LeetcodeTests.swift
+++ LeetcodeTests/LeetcodeTests.swift
@@ -21,7 +21,9 @@ class LeetcodeTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        print(Solution().fullJustify(["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], 20))
+        for i in 0 ... 100 {
+            print(Solution().mySqrt(i))
+        }
     }
     
     func testListNode() throws {
