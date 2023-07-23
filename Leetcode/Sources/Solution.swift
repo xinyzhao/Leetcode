52b99bd7df618cc678440afb12268aa2fef83549
xinyzhao <xinyzhao@qq.com>
2023-07-23 23:19:47 +0800

    Add generate & getRow

--- Leetcode/Sources/Solution.swift
+++ Leetcode/Sources/Solution.swift
@@ -2966,6 +2966,26 @@ class Solution {
         }
         return root
     }
+ 
+    func generate(_ numRows: Int) -> [[Int]] {
+        var rows = [[Int]]()
+        for i in 0 ..< numRows {
+            var row = [Int]()
+            for j in 0 ... i {
+                if j == 0 || j == i {
+                    row.append(1)
+                } else {
+                    row.append(rows[i - 1][j - 1] + rows[i - 1][j])
+                }
+            }
+            rows.append(row)
+        }
+        return rows
+    }
     
+    func getRow(_ rowIndex: Int) -> [Int] {
+        let rows = generate(rowIndex + 1)
+        return rows[rowIndex]
+    }
 }
 
--- LeetcodeTests/LeetcodeTests.swift
+++ LeetcodeTests/LeetcodeTests.swift
@@ -21,7 +21,7 @@ class LeetcodeTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        print(Solution().numDistinct("rabbbit", "rabbit"))
+        print(Solution().generate(5))
     }
     
     func testNode() throws {
